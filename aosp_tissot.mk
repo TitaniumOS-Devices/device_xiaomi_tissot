b488ad8363d4e63b9d68c0e8d306737b42258aac
Abhinavgupta371 <abhinavgupta371@gmail.com>
2019-12-02 04:40:37 +0000

    tissot: Fallback to oreo fp to fix failing CTS

--- aosp_tissot.mk
+++ aosp_tissot.mk
@@ -38,3 +38,13 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 # PixelExperience Stuffs
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
+IS_PHONE := true
+
+#Official coming soon maibe?
+AOSIP_BUILDTYPE := Official
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
